e8f3a5da68cfb0836a72cb39c9c32065c9b2e507
tmarci <tormartonz@gmail.com>
2018-03-13 10:15:29 +0100

    added Game class

---
+++ com/codecool/game/Game.java
@@ -0,0 +1,15 @@
+package com.codecool.game;
+import com.codecool.termlib.Terminal;
+
+public class Game {
+    public static void main(String[] args) {
+        Terminal ter = new Terminal();
+        ter.clearScreen();
+        System.out.print("yyyyyy");
+    }
+
+    public void initBoard() {
+        int[][] grid = new int[10][10];
+        
+    }
+}
