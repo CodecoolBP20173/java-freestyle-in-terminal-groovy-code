395501e4622feb95ff4c0559752d443cf282baf2
Márton Lipécz <mlipecz@gmail.com>
2018-03-13 10:14:50 +0100

    termlib functions ready to test

---
+++ com/codecool/game/Game.java
@@ -0,0 +1,10 @@
+package com.codecool.game;
+import com.codecool.termlib.*;
+
+public class Game{
+    public static void main(String[] args) {
+        Terminal t = new Terminal();
+        t.setBgColor(Color.YELLOW);
+        t.clearScreen();
+    }
+}
