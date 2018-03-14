ba14f53ddf9ab960e9d76567995a8daa746ba285
tmarci <tormartonz@gmail.com>
2018-03-14 10:13:21 +0100

    basic move function implemented

--- com/codecool/game/Game.java
+++ com/codecool/game/Game.java
@@ -1,24 +1,27 @@
 package com.codecool.game;
-
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-
 import javax.lang.model.util.ElementFilter;
-
 import java.io.*;
 import com.codecool.termlib.Color;
+import com.codecool.termlib.Direction;
 import com.codecool.termlib.Terminal;
 
 public class Game {
+    char[][] grid = new char[24][80];
     public static void main(String[] args) {
+        Terminal t = new Terminal();
+        t.moveTo(80, 24);
         while (true) {
-            Terminal t = new Terminal();
-            t.clearScreen();
             Character d = tryToRead();
             if (d == null)
                 continue;
             if (d == 'd') {
-                t.moveTo(30,30);
+                t.setChar(' ');
+                t.moveCursor(Direction.FORWARD, 1);
+                t.setChar('X');
+            }
+            if (d == 'a') {
+                t.setChar(' ');
+                t.moveCursor(Direction.BACKWARD, 1);
                 t.setChar('X');
             }
             if (d == 'p') {
@@ -26,6 +29,11 @@ public class Game {
             }
         }
     }
+
+    public void updateGrid(int x, int y, char c) {
+        grid[5][20] = 'x';
+    }
+
 
     private static Character tryToRead() {
         try {
